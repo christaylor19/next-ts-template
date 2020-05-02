f0dca18eb745ff6ad15683335c6ba8eda825e9e5
christaylor19 <christaylormail.94@gmail.com>
2020-05-02 21:16:57 +0100

    Install ESLint & get working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return <div>Welcome to Next.js!</div>;
 }
 
-export default HomePage
+export default HomePage;
